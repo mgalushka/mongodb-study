bbb83d058644e3ff691435d550bd199de6c387d0
mgalushka@gmail.com <mgalushka@gmail.com@02d571e8-2e2c-f3a2-4a60-98e33c93857a>
2011-07-12 21:01:34 +0000

    added more bus stops on street export

--- trunk/src/test/java/org/osm/project/router/AstarRouter.java
+++ trunk/src/test/java/org/osm/project/router/AstarRouter.java
@@ -4,12 +4,10 @@ import org.openstreetmap.osm.data.MemoryDataSet;
 import org.openstreetmap.osm.data.NodeHelper;
 import org.openstreetmap.osm.data.coordinates.LatLon;
 import org.openstreetmap.osm.io.FileLoader;
-//import org.openstreetmap.osmosis.core.domain.v0_6.Node;
+import org.openstreetmap.osmosis.core.domain.v0_6.Node;
 import org.openstreetmap.travelingsalesman.routing.IRouter;
 import org.openstreetmap.travelingsalesman.routing.Route;
-import org.openstreetmap.travelingsalesman.routing.routers.DepthFirstRouter;
 import org.openstreetmap.travelingsalesman.routing.routers.DijkstraRouter;
-import org.openstreetmap.travelingsalesman.routing.routers.TurnRestrictedMultiTargetDijkstraRouter;
 import org.openstreetmap.travelingsalesman.routing.selectors.Motorcar;
 
 import java.io.File;
@@ -31,19 +29,19 @@ public class AstarRouter {
 //        MemoryDataSet map = (new org.openstreetmap.osm.io.BoundingBoxDownloader(lat0, lon0, lat1, lon1)).parseOsm();
 
         LatLon startCoord = new LatLon(50.4311977, 30.5948412);
-//        Node startNode = NodeHelper.findNearestNode(map, startCoord);
+        Node startNode = NodeHelper.findNearestNode(map, startCoord);
 
         LatLon targetCoord = new LatLon(50.4351546, 30.613116);
 
-//        Node targetNode = NodeHelper.findNearestNode(map, targetCoord);
+        Node targetNode = NodeHelper.findNearestNode(map, targetCoord);
 
         IRouter router = new DijkstraRouter();
-//        Route theRoute = router.route(map, targetNode, startNode, new Motorcar());
+        Route theRoute = router.route(map, targetNode, startNode, new Motorcar());
 
-//        for(Route.RoutingStep rs: theRoute.getRoutingSteps()){
-//            System.out.println(String.format("Coordinates: %f - %f, %f metres",
-//                    rs.getStartNode().getLatitude(), rs.getStartNode().getLongitude(), rs.distanceInMeters()));
-//        }
+        for(Route.RoutingStep rs: theRoute.getRoutingSteps()){
+            System.out.println(String.format("Coordinates: %f - %f, %f metres",
+                    rs.getStartNode().getLatitude(), rs.getStartNode().getLongitude(), rs.distanceInMeters()));
+        }
 
     }
 }
