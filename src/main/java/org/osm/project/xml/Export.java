ca50786f468491a56d7fdd74b4b5bc723d5984c7
mgalushka@gmail.com <mgalushka@gmail.com@02d571e8-2e2c-f3a2-4a60-98e33c93857a>
2011-07-12 06:38:34 +0000

    export improved with finding all relations  for bus stops ways/nodes

--- src/main/java/org/osm/project/xml/Export.java
+++ src/main/java/org/osm/project/xml/Export.java
@@ -7,6 +7,7 @@ import com.google.code.morphia.Datastore;
 import com.google.code.morphia.query.Query;
 import org.osm.project.model.Entity;
 import org.osm.project.model.Node;
+import org.osm.project.model.Relation;
 import org.osm.project.model.Way;
 import org.osm.project.xml.renderers.RenderException;
 
@@ -23,6 +24,18 @@ import java.util.Set;
 public class Export {
 
 
+    private static long startMillis = 0L;
+
+    private static void startTimer(){
+        startMillis = System.currentTimeMillis();
+    }
+
+    private static void printEllapsed(String title){
+        double seconds = (System.currentTimeMillis() - startMillis)/1000.;
+        System.out.printf("%s elapsed in: %f sec.\n", title, seconds);
+        startMillis = System.currentTimeMillis();
+    }
+
     public static void main(String[] args) throws RenderException, StorageException {
         ModelRenderer mr = new ModelRenderer();
         File output = new File("E:\\Projects\\mongodb-study\\examples\\out-test.osm");
@@ -32,16 +45,50 @@ public class Export {
 
         List<Entity> forExport = new ArrayList<Entity>();
 
+        startTimer();
         List<Node> busStops = mongo.find(Node.class, "tags.highway", "bus_stop").asList();
+        printEllapsed("Found all bus stops");
 
+        // find all ways for all stops
         Set<Way> busWays = new HashSet<Way>();
         for(Node n : busStops){
-            busWays.addAll(mongo.createQuery(Way.class).disableValidation().filter("nodes.$id", n.getId()).asList());
+            busWays.addAll(mongo.createQuery(Way.class).disableValidation()
+                    .filter("nodes.$id", n.getId()).asList());
         }
+        printEllapsed("Found ways for all bus stops");
+
+        // find all relations for all ways found and adding them to map too
+        Set<Relation> busWaysRelations = new HashSet<Relation>();
+        for(Way w : busWays){
+            Query<Relation> allBusRelations = mongo.createQuery(Relation.class).disableValidation();
+            allBusRelations.criteria("members.ref").equal(w.getId()).and()
+                    .or(
+                        allBusRelations.criteria("tags.type").equal("associatedStreet"),
+                        allBusRelations.criteria("tags.type").equal("street")
+                    );
+
+            busWaysRelations.addAll(allBusRelations.asList());
+        }
+        printEllapsed("Found relations for all bus stops ways");
+
+        for(Node n : busStops){
+            Query<Relation> allBusStopsRelations = mongo.createQuery(Relation.class).disableValidation();
+            allBusStopsRelations.criteria("members.ref").equal(n.getId()).and()
+                    .or(
+                        allBusStopsRelations.criteria("tags.type").equal("associatedStreet"),
+                        allBusStopsRelations.criteria("tags.type").equal("street")
+                    );
+
+            busWaysRelations.addAll(allBusStopsRelations.asList());
+        }
+        printEllapsed("Found relations for all bus stop nodes");
+
 
         forExport.addAll(busStops);
         forExport.addAll(busWays);
+        forExport.addAll(busWaysRelations);
 
         mr.buildOsm(forExport, output);
+        printEllapsed("Exported");
     }
 }
